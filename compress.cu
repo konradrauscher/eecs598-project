e25d42765a1c73dbc41ff84cc5501f1fe1c1e06e
frenchmatt <mgfrench@umich.edu>
2021-11-30 23:00:02 -0500

    reduced quality factor to 50% as is default, finished file output except image bitstream

--- compress.cu
+++ compress.cu
@@ -269,6 +269,17 @@ private:
         21, 34, 37, 47, 50, 56, 59, 61,
         35, 36, 48, 49, 57, 58, 62, 63  
     };
+    // Mapping of order of quantization tables to be streamed into file
+    const uint8_t ZigZagInv[64] = {  
+        0,  1,  8,  16, 9,  2,  3,  10,
+        17, 24, 32, 25, 18, 11, 4,  5,
+        12, 19, 26, 33, 40, 48, 41, 34,
+        27, 20, 13, 6,  7,  14, 21, 28,
+        35, 42, 49, 56, 57, 50, 43, 36,
+        29, 22, 15, 23, 30, 37, 44, 51,
+        58, 59, 52, 45, 38, 31, 39, 46,
+        53, 60, 61, 54, 47, 55, 62, 63 
+    };
     // Huffman definitions for first DC/AC tables (luminance / Y channel)
     const uint8_t DcLuminanceCodesPerBitsize[16]   = { 0,1,5,1,1,1,1,1,1,0,0,0,0,0,0,0 };   // sum = 12
     const uint8_t DcLuminanceValues         [12]   = { 0,1,2,3,4,5,6,7,8,9,10,11 };         // => 12 codes
@@ -367,12 +378,16 @@ void Compressor::write_file() {
     // Quantization Tables
     bitWriter.addMarker(0xDB, 2 + 2*(1 + 8*8));
     bitWriter   << 0x00;
-    for (auto i = 0; i < 8; i++) {
-        bitWriter << Q_l[i];
+    for (auto i = 0; i < 64; i++) {
+        auto y = ZigZagInv[i] / 8;
+        auto x = ZigZagInv[i] % 8;
+        bitWriter << Q_l[y][x];
     }
     bitWriter   << 0x01;
-    for (auto i = 0; i < 8; i++) {
-        bitWriter << Q_c[i];
+    for (auto i = 0; i < 64; i++) {
+        auto y = ZigZagInv[i] / 8;
+        auto x = ZigZagInv[i] % 8;
+        bitWriter << Q_c[y][x];
     }
 
     // Bits/Pixel, Image Size, Number of Channels, and Subsampling and Y vs C for each channel
